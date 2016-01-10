88415a6a284d75ad25da525c3e42b1ed5e5246c5
Matt Winckler <himself@mattwinckler.com>
2016-01-09 20:06:12 -0800

    More hacking around bbpress

--- content.php
+++ content.php
@@ -4,7 +4,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php 
-		$is_page = get_post_type() == 'page';
+		$post_type = get_post_type();
+		$is_page = $post_type == 'page';
+		$is_forum = $post_type == 'forum' || $post_type = 'topic';
 
 		if ( is_single() ) {
 			the_title( '<h1>', '</h1>' );
@@ -12,7 +14,7 @@
 			the_title( sprintf( '<h1><a href="%s">', esc_url( get_permalink() ) ), '</a></h1>' );
 		}
 
-		if ( !$is_page ): ?>
+		if ( !$is_page && !$is_forum ): ?>
 			<div class="author-byline">
 				<time><?php the_time( 'F jS, Y' ); ?></time> 
 				&SmallCircle; 
@@ -34,7 +36,7 @@
 
 		the_content( __( 'Continue reading...', 'simplecommerce' ) );
 
-		if ( get_post_type() != 'page' && get_post_type() != 'forum' ) {
+		if ( !$is_page && !$is_forum ) {
 			simplecommerce_author_box();
 		}
 
