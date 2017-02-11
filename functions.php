252292d4a39dbe0a40d74d63dcbd4e869d5452fd
Matt Winckler <matt.winckler@gmail.com>
2017-02-11 12:10:04 -0800

    Add WooCommerce support

--- functions.php
+++ functions.php
@@ -88,6 +88,30 @@ add_action( 'widgets_init', function() {
 });
 
 // ==========================================================
+// WooCommerce
+// ==========================================================
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+
+add_action( 'woocommerce_before_main_content', 'woocommerce_theme_wrapper_start', 10 );
+add_action( 'woocommerce_after_main_content', 'woocommerce_theme_wrapper_end', 10 );
+
+function woocommerce_theme_wrapper_start() {
+	echo '<div class="row"><div class="twelve columns">';
+}
+
+function woocommerce_theme_wrapper_end() {
+	echo '</div></div>';
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+// ==========================================================
 // Shortcodes
 // ==========================================================
 
